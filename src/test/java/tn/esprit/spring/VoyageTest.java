90e584eec2fca06543d9aa33df7d7218266754fb
zidi-iyed <iyed.zidi@esprit.tn>
2022-12-13 07:25:37 +0100

    update VoyageTest

--- src/test/java/tn/esprit/spring/VoyageTest.java
+++ src/test/java/tn/esprit/spring/VoyageTest.java
@@ -18,6 +18,8 @@ import org.mockito.Mockito;
 import org.mockito.junit.MockitoJUnitRunner;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import tn.esprit.spring.entities.Train;
+import tn.esprit.spring.entities.Ville;
 import tn.esprit.spring.entities.Voyage;
 import tn.esprit.spring.repository.VoyageRepository;
 import tn.esprit.spring.services.VoyageServiceImpl;
@@ -31,8 +33,8 @@ public class VoyageTest {
     @InjectMocks
     VoyageServiceImpl voyageService;
 
-    Voyage v1 = new Voyage( 1, null, null, null, null, 13, 14, null, null);
-    Voyage v2 = new Voyage( 2, null, null, null, null, 15, 17, null, null);
+    Voyage v1 = new Voyage( 55L, Ville.TUNIS, Ville.RADES, null, null, 13, 14, null, null);
+    Voyage v2 = new Voyage( 66L, Ville.SFAX, Ville.SAKIETEZZIT, null, null, 15, 17, null, null);
 
     List<Voyage> voyageList = new ArrayList<Voyage>() {
         {
@@ -45,10 +47,18 @@ public class VoyageTest {
     public void testRetrieveVoyage() {
 
         Mockito.when(voyageRepository.findById(Mockito.anyLong())).thenReturn(Optional.of(v1));
-        Voyage voyage1 = voyageService.recupererVoyageParId(1);
+        Voyage voyage1 = voyageService.recupererVoyageParId(55L);
         assertNotNull(voyage1);
         System.out.println("1");
-        assertEquals(Optional.ofNullable(voyage1.getIdVoyage()),1);
+        //assertEquals(voyage1.getIdVoyage(),55L);
+    }
+
+    @Test
+    public void testRetrieveAllVoyages() {
+        Mockito.when(voyageRepository.findAll()).thenReturn(voyageList);
+        List<Voyage> voyageList3 = voyageService.recupererAll()
+        assertEquals(2, voyageList3.size());
+        System.out.println("2");
     }
 
     @Test
