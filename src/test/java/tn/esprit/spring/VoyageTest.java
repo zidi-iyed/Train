506b6a902aa532b2230eaa8c6a59e6f74d36d204
zidi-iyed <iyed.zidi@esprit.tn>
2022-12-13 07:47:23 +0100

    update VoyageTest

--- src/test/java/tn/esprit/spring/VoyageTest.java
+++ src/test/java/tn/esprit/spring/VoyageTest.java
@@ -72,7 +72,7 @@ public class VoyageTest {
 
     @Test
     public void testDeleteVoyage(){
-        Voyage voyage1 = voyageService.recupererVoyageParId(66L);
+        Voyage voyage1 = v1;
         voyageService.supprimerVoyage(voyage1);
         Mockito.verify(voyageRepository, times(1)).deleteById(66L);
         System.out.println("4");
